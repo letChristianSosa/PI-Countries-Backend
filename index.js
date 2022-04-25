f14f4567fa37f0993d0be3382e01912e09d7a556
Christian Sosa <chrissm28@outlook.com>
2022-04-25 17:54:15 -0500

    Added env PORT

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ createDB();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
