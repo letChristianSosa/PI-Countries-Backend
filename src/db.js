9d9f1d3f85240d7a9e74b0c6a41cd9d578884538
Christian Sosa <chrissm28@outlook.com>
2022-04-25 21:02:33 -0500

    g

--- src/db.js
+++ src/db.js
@@ -7,6 +7,12 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
