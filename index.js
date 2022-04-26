87f3869a0578c8761cdc49f51fe865b6f0efcdf4
Christian Sosa <chrissm28@outlook.com>
2022-04-26 14:39:39 -0500

    Deployment

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ createDB();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.DB_PORT, () => {
-    console.log(`%s listening at ${process.env.DB_PORT}`); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
